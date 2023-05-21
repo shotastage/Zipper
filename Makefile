45968575c798da24cdabfd982dd87e5ca5a431e6
shotastage <hornet.live.mf@gmail.com>
2023-05-21 14:39:01 +0900

    add setup makefile

---
+++ Makefile
@@ -0,0 +1,6 @@
+.PHONY
+rust-setup:
+	rustup target add aarch64-linux-android
+	rustup target add armv7-linux-androideabi
+	rustup target add i686-linux-android
+	rustup target add x86_64-linux-android
--- app/src/main/rust/zipper/src/archiver.rs
+++ app/src/main/rust/zipper/src/archiver.rs
@@ -1,5 +1,29 @@
-pub fn compress(left: usize, right: usize) -> usize {
-    left + right
+use std::io::prelude::*;
+use flate2::Compression;
+use flate2::write::ZlibEncoder;
+use std::io::prelude::*;
+use flate2::read::GzDecoder;
+
+use jni::objects::JObject;
+use jni::sys::jstring;
+use jni::JNIEnv;
+
+
+#[no_mangle]
+pub unsafe extern "C" fn native_zlib_compress() {
+    //
+}
+
+pub fn compress_zip_b() {
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+    e.write_all(b"foo");
+    let compressed_bytes = e.finish();
+}
+
+pub fn decompress_b() {
+    let mut d = GzDecoder::new("...".as_bytes());
+    let mut s = String::new();
+    d.read_to_string(&mut s).unwrap();
 }
 
 #[cfg(test)]
